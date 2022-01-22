e5c1634b01be20086e0f54ce0637d036f5715cc8
Artyom Arnaut <artyomarnaut@MBP-Artyom.Dlink>
2022-01-22 03:49:23 +0300

    desktopVersionMastersSlider

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 2,
+    spaceBetween: 30,
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
